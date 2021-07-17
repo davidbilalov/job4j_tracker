97c6c3812cdaa2dc1c6cada304f4569c9a7a033b
daidb <davidbilalov94@mail.ru>
2021-07-17 20:53:12 +0300

    1.2. Вызов метода объекта [#166085]

---
+++ src/main/java/ru/job4j/tracker/ru/job4j/oop/Student.java
@@ -0,0 +1,12 @@
+package ru.job4j.tracker.ru.job4j.oop;
+
+public class Student {
+    public void song() {
+        System.out.println("I believe I can fly");
+    }
+
+    public static void main(String[] args) {
+        Student david = new Student();
+        david.song();
+    }
+}
