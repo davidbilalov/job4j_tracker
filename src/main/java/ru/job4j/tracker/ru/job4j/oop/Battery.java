226863958aacb984c33747b9abc065865cf85fc8
daidb <davidbilalov94@mail.ru>
2021-07-19 15:10:48 +0300

    1.8. Состояние объекта [#167388]

---
+++ src/main/java/ru/job4j/tracker/ru/job4j/oop/Battery.java
@@ -0,0 +1,16 @@
+package ru.job4j.tracker.ru.job4j.oop;
+
+public class Battery {
+    private int load;
+    public Battery(int load) {
+        this.load = load;
+    }
+    public void exchange(Battery another) {
+        this.load = this.load + another.load;
+        another.load = 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
