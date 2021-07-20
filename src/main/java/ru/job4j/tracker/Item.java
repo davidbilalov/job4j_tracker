b4484a06e4a0f3224b9e7edec373b7cc3d71656f
daidb <davidbilalov94@mail.ru>
2021-07-21 01:00:16 +0300

    2. Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -5,18 +5,34 @@ public class Item {
     private String name;
 
     public int getId() {
+
         return id;
     }
 
     public void setId(int id) {
+
         this.id = id;
     }
 
     public String getName() {
+
         return name;
     }
 
+    public Item() {
+
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
     public void setName(String name) {
+
         this.name = name;
     }
 }
