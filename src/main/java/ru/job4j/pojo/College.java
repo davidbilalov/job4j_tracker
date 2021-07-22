e5a16b35237e4fe42f9794d5a41a602edd9a05f9
daidb <davidbilalov94@mail.ru>
2021-07-22 17:22:48 +0300

    2. Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,11 @@
+package ru.job4j.pojo;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setDate("10");
+        student.setFio("D B R");
+        student.setGroup("2b");
+        System.out.println(" date: " + student.getDate() + "  Fio: " + student.getFio() + " group: " + student.getGroup() );
+    }
+}
