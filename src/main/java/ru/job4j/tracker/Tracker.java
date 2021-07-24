469c4d57ea1892443b7b789b04316a6664b8b9a2
daidb <davidbilalov94@mail.ru>
2021-07-25 00:06:43 +0300

    5. Tracker - хранилище [#396 #181249]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,6 @@
 package ru.job4j.tracker;
 
+import  java.util.Arrays;
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -24,11 +25,31 @@ public class Tracker {
     }
 
     public Item[] findByName(String key) {
-        return null;
+        int count  = 0;
+        Item[] list = new Item[size];
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (item.getName().equals(key)) {
+                list[count] = item;
+                count++;
+            }
+        }
+        Item[] listreturn =  Arrays.copyOf(list, count);
+
+        return listreturn;
     }
 
     public Item[] findAll() {
-        return null;
+        int count = 0;
+        Item[] list = new Item[size];
+        for (int index = 0; index < size; index++) {
+            if (!(items[index].equals(null))) {
+                list[count] = items[index];
+                count++;
+            }
+        }
+        Item[] listReturn = Arrays.copyOf(list, count);
+        return listReturn;
     }
 //    получение списка всех заявок - public Item[] findAll();
 //    получение списка по имени - public Item[] findByName(String key);
