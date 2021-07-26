4fdcb6dae7391d769ab829ebf1100efca0ea2ef8
daidb <davidbilalov94@mail.ru>
2021-07-26 23:39:42 +0300

    9. toString [#310018]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,12 @@
+package ru.job4j.tracker;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item item = new Item();
+        System.out.println(item);
+    }
+
+
+}
