7af843c768599ca1c42cf1486a96969b9962860b
daidb <davidbilalov94@mail.ru>
2021-07-27 22:02:15 +0300

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport{
+    @Override
+    public String ride(String ride) {
+        return null;
+    }
+
+    @Override
+    public int passengers(String passengers) {
+        return 0;
+    }
+
+    @Override
+    public int price(int price) {
+        return 0;
+    }
+}
---
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -0,0 +1,9 @@
+package ru.job4j.poly;
+
+public interface Transport {
+    String ride(String ride);
+
+    int passengers(String passengers);
+
+    int price(int price);
+}
