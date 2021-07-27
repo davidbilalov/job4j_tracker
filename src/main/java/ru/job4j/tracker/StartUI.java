494018c30ed00cd4906a036243c33b7910e169f9
daidb <davidbilalov94@mail.ru>
2021-07-28 00:30:23 +0300

    4.2. Статические методы. [#181779]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -3,22 +3,31 @@ package ru.job4j.tracker;
 import java.util.Arrays;
 
 public class StartUI {
+    public static void createItem(Input input, Tracker tracker) {
+        System.out.println("=== Create a new Item ====");
+        String name = input.askStr("Enter name: ");
+        Item item = new Item(name);
+        tracker.add(item);
+    }
+
+    public static void replace(Input input, Tracker tracker) {
+        int id = Integer.parseInt(input.askStr("=== Edit item ===="));
+        System.out.print("Enter name: ");
+        String name = input.askStr("");
+        Item item = new Item(name);
+    }
+
+    public static void delete(Input input) {
+        int id = Integer.parseInt(input.askStr("Enter id: "));
+    }
 
     public void init(Input input, Tracker tracker) {
-        boolean run = true;
-        while (run) {
-            showMenu();
-            String msg = "Enter string: ";
-            String enterId = "Enter id: ";
-            int select = Integer.parseInt(input.askStr(msg));
-
-            if (select == 0) {
-                System.out.println("=== Create a new Item ====");
-                System.out.print("Enter name: ");
-                String name = input.askStr(msg);
-                Item item = new Item(name);
-                tracker.add(item);
-                System.out.println("Добавленная заявка: " + item);
+            boolean run = true;
+            while (run) {
+                this.showMenu();
+                int select = input.askInt("Select: ");
+                if (select == 0) {
+                    StartUI.createItem(input, tracker);
             } else if (select == 1) {
                 System.out.println("=== Show all items ====");
                 Item[] items = tracker.findAll();
@@ -30,30 +39,14 @@ public class StartUI {
             } else if (select == 6) {
                 run = false;
             } else if (select == 2) {
-                System.out.println("=== Edit item ====");
-                System.out.print("Enter id: ");
-                int id = Integer.parseInt(input.askStr(enterId));
-                System.out.print("Enter name: ");
-                String name = input.askStr(msg);
-                Item item = new Item(name);
-                if (tracker.replace(id, item)) {
-                    System.out.println("Заявка изменена успешно.");
-                } else {
-                    System.out.println("Ошибка замены заявки.");
-                }
+                StartUI.replace(input, tracker);
             } else if (select == 3) {
-                System.out.println("=== Delete item ====");
-                System.out.print("Enter id: ");
-                int id = Integer.parseInt(input.askStr(enterId));
-                if (tracker.delete(id)) {
-                    System.out.println("Заявка удалена успешно.");
-                } else {
-                    System.out.println("Ошибка удаления заявки.");
-                }
+                    StartUI.delete(input);
+
             } else if (select == 4) {
                 System.out.println("=== Find item by id ====");
                 System.out.print("Enter id: ");
-                int id = Integer.parseInt(input.askStr(enterId));
+                int id = Integer.parseInt(input.askStr("Enter id: "));
                 Item item = tracker.findById(id);
                 if (item != null) {
                     System.out.println(item);
@@ -63,7 +56,7 @@ public class StartUI {
             } else if (select == 5) {
                 System.out.println("=== Find items by name ====");
                 System.out.print("Enter name: ");
-                String name = input.askStr(enterId);
+                String name = input.askStr("Enter id: ");
                 Item[] items = tracker.findByName(name);
                 if (items.length > 0) {
                     for (Item item : items) {
