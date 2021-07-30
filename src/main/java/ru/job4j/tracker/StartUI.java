e4fe42e1835ccae07dcf7f41bf79a59a5be2dea9
daidb <davidbilalov94@mail.ru>
2021-07-30 19:16:15 +0300

    4.1. Разрыв зависимости StartUI от Scanner. [#181778 #182569]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -4,44 +4,23 @@ import java.util.Arrays;
 
 public class StartUI {
     public static void createItem(Input input, Tracker tracker) {
-        System.out.println("=== Create a new Item ===");
+        System.out.println("=== Create a new Item ====");
         String name = input.askStr("Enter name: ");
         Item item = new Item(name);
         tracker.add(item);
     }
-
-    public static void findAllItems(Input input, Tracker tracker) {
-        System.out.println("=== Showing all items ===" + System.lineSeparator());
-        System.out.println(Arrays.toString(tracker.findAll()));
-    }
-
-    public static void editItem(Input input, Tracker tracker) {
-        System.out.println("=== Edit an item ===" + System.lineSeparator());
-        int id = Integer.parseInt(input.askStr("Enter ID to replace: "));
-        System.out.println();
-        String name = input.askStr("Enter name: ");
+    public static void replace(Input input, Tracker tracker) {
+        int id = Integer.parseInt(input.askStr("=== Edit item ===="));
+        System.out.print("Enter name: ");
+        String name = input.askStr("");
         Item item = new Item(name);
-        tracker.replace(id, item);
     }
 
-    public static void deleteItem(Input input, Tracker tracker) {
-        System.out.println("=== Delete an item ===" + System.lineSeparator());
-        int id = Integer.parseInt(input.askStr("Enter item's ID: "));
-        tracker.delete(id);
+    public static void delete(Input input) {
+        int id = input.askInt("Enter id: ");
     }
 
-    public static void findItemByID(Input input, Tracker tracker) {
-        System.out.println("=== Find an item by ID ===" + System.lineSeparator());
-        int id = Integer.parseInt(input.askStr("Enter ID: "));
-        tracker.findById(id);
-    }
-
-    public static void findItemByName(Input input, Tracker tracker) {
-        System.out.println("=== Find an item by Name ===" + System.lineSeparator());
-        String name = input.askStr("Enter name: ");
-        tracker.findByName(name);
-    }
-    public void init(Input input, Tracker tracker, UserAction[] actions) {
+    public void init(Input input, Tracker tracker) {
         boolean run = true;
         while (run) {
             this.showMenu();
@@ -49,41 +28,60 @@ public class StartUI {
             if (select == 0) {
                 StartUI.createItem(input, tracker);
             } else if (select == 1) {
-                StartUI.findAllItems(input, tracker);
-            } else if (select == 2) {
-                StartUI.editItem(input, tracker);
+                System.out.println("=== Show all items ====");
+                Item[] items = tracker.findAll();
+                if (items.length > 0) {
+                    for (Item item : items) {
+                        System.out.println(item);
+                    }
+                } else {
+                    System.out.println("Хранилище еще не содержит заявок");
+                }
+            }  else if (select == 2) {
+                StartUI.replace(input, tracker);
             } else if (select == 3) {
-                StartUI.deleteItem(input, tracker);
-
+                StartUI.delete(input);
             } else if (select == 4) {
-                StartUI.findItemByID(input, tracker);
+                System.out.println("=== Find item by id ====");
+                System.out.print("Enter id: ");
+                int id = input.askInt("Enter id: ");
+                Item item = tracker.findById(id);
+                if (item != null) {
+                    System.out.println(item);
+                } else {
+                    System.out.println("Заявка с введенным id: " + id + " не найдена.");
+                }
             } else if (select == 5) {
-                StartUI.findItemByName(input, tracker);
+                System.out.println("=== Find items by name ====");
+                System.out.print("Enter name: ");
+                String name = input.askStr("Enter id: ");
+                Item[] items = tracker.findByName(name);
+                if (items.length > 0) {
+                    for (Item item : items) {
+                        System.out.println(item);
+                    }
+                } else {
+                    System.out.println("Заявки с именем: " + name + " не найдены.");
+                }
             } else if (select == 6) {
                 run = false;
             }
         }
     }
-
-            private void showMenu () {
-                String[] menu = {
-                        "Add new Item", "Show all items", "Edit item",
-                        "Delete item", "Find item by id", "Find items by name",
-                        "Exit Program"
-                };
-                System.out.println("Menu:");
-                for (int i = 0; i < menu.length; i++) {
-                    System.out.println(i + ". " + menu[i]);
-                }
-            }
-
-            public static void main (String[]args){
-                Input input = new ConsoleInput();
-                Tracker tracker = new Tracker();
-                UserAction[] actions = {
-                        new CreateAction(), new DeleteAction(), new ReplaceAction()
-                };
-                new StartUI().init(input, tracker, actions);
-            }
+    private void showMenu() {
+        String[] menu = {
+                "Add new Item", "Show all items", "Edit item",
+                "Delete item", "Find item by id", "Find items by name",
+                "Exit Program"
+        };
+        System.out.println("Menu:");
+        for (int i = 0; i < menu.length; i++) {
+            System.out.println(i + ". " + menu[i]);
         }
-
+    }
+    public static void main(String[] args) {
+        Input input = new ConsoleInput();
+        Tracker tracker = new Tracker();
+        new StartUI().init(input, tracker);
+    }
+}
