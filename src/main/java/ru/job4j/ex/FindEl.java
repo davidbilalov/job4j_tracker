d9fa1c162551a35bbfb563f681fdae460561e78a
daidb <davidbilalov94@mail.ru>
2021-08-02 01:35:51 +0300

    0.4. Пользовательские исключения. [#219367 #183419]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -2,14 +2,15 @@ package ru.job4j.ex;
 
 public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
-        if (key == null) {
-            throw new ElementNotFoundException("Element not found.");
-        }
         int rsl = -1;
         for (int index = 0; index < value.length; index++) {
             if (value[index].equals(key)) {
                 rsl = index;
+                break;
             }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("Element not found.");
         }
         return rsl;
     }
