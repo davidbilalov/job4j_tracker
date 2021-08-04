028a1c5888f14eaeae9a9d867a1e18b6589232b5
daidb <davidbilalov94@mail.ru>
2021-08-04 17:19:53 +0300

    5.1. Тестирование. Подготовка данных. [#182960 #182955]

--- src/main/java/ru/job4j/tracker/StartUITest.java
+++ src/main/java/ru/job4j/tracker/StartUITest.java
@@ -40,6 +40,6 @@ public class StartUITest {
         };
         StartUI.deleteItem(new StubInput(answers), tracker);
         Item deleted = tracker.findById(item.getId());
-        assertNull(deleted.getId());
+         assertNull(deleted);
     }
 }
