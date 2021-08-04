ed611256f5bca816a0c13156e13b894d3620f3c6
daidb <davidbilalov94@mail.ru>
2021-08-04 19:30:13 +0300

    3. Массивы и модели. [#174892 #180841]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -3,9 +3,17 @@ package ru.job4j.pojo;
 public class Library {
     public static void main(String[] args) {
         Book cleancode = new Book();
+        cleancode.setName("Clean code");
+
         Book sun = new Book();
+        sun.setName("Sun");
+
         Book moon = new Book();
+        moon.setName("Moon");
+
         Book star = new Book();
+        star.setName("Star");
+
         Book[] array = new Book[4];
         array[0] = sun;
         array[1] = moon;
@@ -19,5 +27,16 @@ public class Library {
         Book temp = array[0];
         array[0] = array[3];
         array[3] = temp;
+        System.out.println();
+        for(int index = 0; index < array.length; index++) {
+            Book book = array[index];
+            System.out.println(book.getName());
+        }
+        System.out.println();
+        for (int index = 0; index < array.length; index++) {
+            if(array[index] == cleancode) {
+                System.out.println(cleancode.getName());
+            }
+        }
     }
 }
