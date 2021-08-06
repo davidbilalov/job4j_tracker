645629a8ba0c6d353dcb561aabc75b650c64b83f
daidb <davidbilalov94@mail.ru>
2021-08-06 17:36:27 +0300

    4. Переопределение [#174890 #179935]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -5,9 +5,9 @@ public class JSONReport extends TextReport {
     @Override
     public String generate(String name, String body) {
         return "{ " + System.lineSeparator()
-                + "\"name" + "\" : " + "\"name" + "\","
+                + "\"name" + "\" : " + "\"" + name + "\","
                 + System.lineSeparator()
-                + "\"body" + "\" : " + "\"body" + "\""
+                + "\"body" + "\" : " + "\"" + body + "\""
                 + System.lineSeparator() + "}";
     }
 
