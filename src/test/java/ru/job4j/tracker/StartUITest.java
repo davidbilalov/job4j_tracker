55ebee299a8ef2f337f2dd4b9efd7441c4475bfc
daidb <davidbilalov94@mail.ru>
2021-08-09 00:50:22 +0300

    9.1 Зависимость от System.out [#33568 #186598]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -8,10 +8,10 @@ import static org.junit.Assert.assertThat;
 public class StartUITest {
     @Test
     public void whenAddItem() {
+        Output output = new StubOutput();
         String[] answers = {"Fix PC"};
         Input input = new StubInput(answers);
         Tracker tracker = new Tracker();
-        StartUI.createItem(input, tracker);
         Item created = tracker.findAll()[0];
         Item expected = new Item("Fix PC");
         assertThat(created.getName(), is(expected.getName()));
